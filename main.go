7c87b1af15ed06d25b75376a70d4c53528abb28a
Suyash Joshi <suyash.joshi179@gmail.com>
2023-01-29 13:10:31 +0530

    Added Whitelisting of origins functionality

--- main.go
+++ main.go
@@ -1,37 +1,84 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
-func modifyResponse() func(*http.Response) error {
-    return func(resp *http.Response) error {
-        resp.Header.Set("Access-Control-Allow-Origin", "*")
-		fmt.Print(resp.Status)
-        return nil
+var allowedUrls = []string{
+    "",
+	"http://localhost:3000",
+	"https://localhost:3000",
+}
+
+// function to check whether the backendServer parameter is a valid url or not
+func isValidUrl(urlString string) bool {
+    _, err := url.ParseRequestURI(urlString)
+    return err == nil
+}
+
+// function to check whether the request is coming from an allowed URL or not
+func isAllowedUrl(origin string) bool {
+    for _, u := range allowedUrls {
+        if u == origin {
+            return true
+        }
     }
+    return false
 }
 
 func process(c *gin.Context) {
-	remote, err := url.Parse(c.Param("proxyPath")[1:])
+	// Extract the URL from request path
+	backendServer := strings.TrimLeft(c.Param("proxyPath"), "/")
+
+	// check if the URL is valid
+	if backendServer == "" || !isValidUrl(backendServer) {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid backend URL in the path"})
+		return
+	}
+
+	// Check if the request is coming from an allowed Origin
+	origin := c.Request.Header.Get("Origin")
+	// fmt.Println("Origin: ", origin)
+	if !isAllowedUrl(origin) {
+		c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "Unauthorized access"})
+		return
+	}
+	
+	// Create a new proxy
+	remote, err := url.Parse(backendServer)
 	if err != nil {
-		panic(err)
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid backend URL in the path"})
+		return
 	}
 	proxy := httputil.NewSingleHostReverseProxy(remote)
 	proxy.Director = func(req *http.Request) {
 		req.Header = c.Request.Header
+		req.Header.Set("Origin", "")
 		req.Host = remote.Host
 		req.URL.Scheme = remote.Scheme
 		req.URL.Host = remote.Host
 		req.URL.Path = remote.Path
 	}
-	proxy.ModifyResponse = modifyResponse()
 
+	// Add CORS headers to the response
+	c.Header("Access-Control-Allow-Origin", "*")
+	// c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+	// c.Header("Access-Control-Allow-Headers", "Content-Type")
+
+	// checking for unexpected errors
+	defer func() {
+		if r := recover(); r != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected Error Occurred"})
+		}
+	}()
+	
+	// Serve the request via the proxy
 	proxy.ServeHTTP(c.Writer, c.Request)
 }
 
